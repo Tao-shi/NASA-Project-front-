f1ce57e7481b147815bc4e6614f01e68598af0e5
tao-shi <taofiq.subair@gmail.com>
2023-01-16 17:42:23 +0100

    updated base url

--- src/utils/appdetails.ts
+++ src/utils/appdetails.ts
@@ -1,2 +1,3 @@
-export const REACT_APP_BASE_URL = "http://localhost:3001/api";
-export const REACT_APP_NASA_API_KEY = "6P6cs0CaiRSgeKkIuf50HtQ8TtWgfBhsuesnsbQz"
+export const REACT_APP_BASE_URL = "http://18.236.45.98:3001/api";
+export const REACT_APP_NASA_API_KEY =
+  "6P6cs0CaiRSgeKkIuf50HtQ8TtWgfBhsuesnsbQz";
