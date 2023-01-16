fb00da6286f43805d2bc0cf84b3779cac2a43c0b
tao-shi <taofiq.subair@gmail.com>
2023-01-16 19:16:43 +0100

    updated base url

--- src/utils/appdetails.ts
+++ src/utils/appdetails.ts
@@ -1,3 +1,3 @@
-export const REACT_APP_BASE_URL = "http://18.236.45.98:3001/api";
+export const REACT_APP_BASE_URL = "https://pepwater.ng";
 export const REACT_APP_NASA_API_KEY =
   "6P6cs0CaiRSgeKkIuf50HtQ8TtWgfBhsuesnsbQz";
